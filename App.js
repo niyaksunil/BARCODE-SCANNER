3b70be5674582f0c87cfb35185d67d96d0a714b2
niya <niyasunilk@gmail.com>
2021-07-11 15:50:23 +0530

    Created the Barcode-Scanner App

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import {Header} from 'react-native-elements';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import ScanScreen from "./screens/ScanScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      <SafeAreaProvider>
+         <View style={styles.container}>
+         <Header 
+            backgroundColor = {"brown"}
+            centerComponent = {{
+            text:"Barcode Scanner",
+            style :{color:"white",fontSize:20},
+          }}/>
+           <Image 
+            style = {styles.imageIcon}
+            source={{
+              uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Barcode-scanner.jpg/220px-Barcode-scanner.jpg",
+            }}
+          />
+          <ScanScreen />
+        </View>
+      </SafeAreaProvider>
+     
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageIcon:{
+    width : 150,
+    height:150,
+    alignSelf:"center",
+    marginTop : 40
   },
 });
